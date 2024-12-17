65873f2e98e3b25266a75174a159a3e2e07c5277
Sasakiarthur <sasakiarthur10@gmail.com>
2024-12-17 18:46:02 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,21 +56,32 @@ router.get('/', async (req, res) => {
                let b64data = Buffer.from(data).toString('base64');
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
-               let GIFTED_MD_TEXT = `
-┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+               let LUCK_MD_TEXT = `
+┏━━━━━━━━━━━━━━℘
+┃❛♤LUCK_MD♤❜ SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
-┗━━━━━━━━━━━━━━━
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 DAVID CYRIL 𖥘
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-Please Follow My Support Channel
-Wanna talk to me?👉 https://t.me/deecee_x 👈
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-2099 David Cyril*
+┗━━━━━━━━━━━━━━━℘
+༒▬▬▬▬▬▬▬▬▬▬▬▬▬▬༒
+❶ || Creator = 𖥘 ARTHUR SASAKI 𖥘
+༒▬▬▬▬▬▬▬▬▬▬▬▬▬▬༒
+❷ || WhattsApp Channel = https://whatsapp.com/channel/0029Vajrhmz96H4IsEjh4a41
+༒▬▬▬▬▬▬▬▬▬▬▬▬▬▬༒
+───〈 *𝒀𝒐𝒖𝒕𝒖𝒃𝒆 𝑪𝒉𝒂𝒏𝒏𝒆𝒍*〉───◆
+https://www.youtube.com/@SSK-FAMILYCAMPAGNY
+
+┌───〈 *𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑 𝑪𝒉𝒂𝒏𝒏𝒆𝒍*〉───◆
+https://whatsapp.com/channel/0029Vajrhmz96H4IsEjh4a41
+
+┌───〈 *དᵀᵉˡᵉᵍʳᵃᵐ ᶜʰᵃⁿⁿᵉˡ ཌ*〉───◆
+
+https://t.me/sasaki_compagnie
+
+✦•━━━━━━━━━༒Support group༒━━━━━━━━•✦
+━➣ https://chat.whatsapp.com/IdB2EfQiNlKBekQrigN9m9
+                   
+༒▬▬▬▬▬▬▬▬▬▬▬▬▬▬༒
+©*2024-2099 SASAKI COMPAGNY*
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
